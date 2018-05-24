f7252181ffd627046dd9d31950a4416450866596
surya <surya.phani1729@gmail.com>
2018-05-24 12:58:52 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -40,3 +40,13 @@ self.addEventListener('fetch', function(e) {
   );
 });
 
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+});
+
+
